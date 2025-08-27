c69f295077fbbfcc5f56895b66410796b0b79cfd
umer-khan <umerakmalku@gmail.com>
2025-08-27 20:35:56 +0500

    fix add-instance sensitive variable issue

--- scripts/terraform/add-instance/main.tf
+++ scripts/terraform/add-instance/main.tf
@@ -19,7 +19,7 @@ resource "terracurl_request" "add_instance" {
     "Content-Type" = "application/x-www-form-urlencoded"
   }
 
-  request_body = "action=add_instance&template=${var.template}&rid=${var.rid}&qty=1&keys[]=${var.ssh_key_name}&srvrname=${var.server_name}&billingcycle=${var.billingcycle}&location_id=${var.location_id}&API=${var.API}"
+    request_body = format("action=add_instance&template=%s&rid=%d&qty=1&keys[]=%s&srvrname=%s&billingcycle=%s&location_id=%d&API=%s", var.template, var.rid, urlencode(var.ssh_key_name), urlencode(var.server_name), var.billingcycle, var.location_id, var.API)
 
   response_codes = [200]
 }

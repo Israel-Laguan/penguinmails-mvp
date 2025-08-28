61960a62922f68e0e898416c39e9e217d52aa90e
umer-khan <umerakmalku@gmail.com>
2025-08-28 15:21:41 +0500

    complete work done

--- scripts/terraform/add-instance/main.tf
+++ scripts/terraform/add-instance/main.tf
@@ -19,7 +19,7 @@ resource "terracurl_request" "add_instance" {
     "Content-Type" = "application/x-www-form-urlencoded"
   }
 
-  request_body = format("action=add_instance&template=%s&rid=%d&qty=1&keys[]=%s&srvrname=%s&billingcycle=%s&location_id=%d&API=%s", var.template, var.rid, urlencode(var.ssh_key_name), urlencode(var.server_name), var.billingcycle, var.location_id, urlencode(var.API))
+    request_body = format("action=add_instance&template=%s&rid=%d&qty=1&keys[]=%s&srvrname=%s&billingcycle=%s&location_id=%d&API=%s", urlencode(var.template), var.rid, urlencode(var.ssh_key_name), urlencode(var.server_name), var.billingcycle, var.location_id, urlencode(var.API))
 
   response_codes = [200]
 }
